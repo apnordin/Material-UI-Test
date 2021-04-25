74f56e31594b747a3526dd9b36acdab756a3ce3c
Alex Nordin <apnordin@gmail.com>
2021-04-25 11:21:29 -0400

    Grid test w/ paper and w/ color-change-button

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,7 +1,22 @@
 import React, { useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
 import Button from "@material-ui/core/Button";
 
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+    },
+    paper: {
+        padding: theme.spacing(2),
+        textAlign: 'center',
+        color: theme.palette.text.secondary,
+    }
+}));
+
 export default function Main () {
+    const classes = useStyles();
 
     const [btnColor, setBtnColor]= useState("primary");
 
@@ -14,8 +29,63 @@ export default function Main () {
     }
 
     return (
-        <Button id="hello-world" variant="contained" color={btnColor} onClick={changeColor}>
-            Change colors!
-        </Button>
+        <div className={classes.root}>
+            <Grid container spacing={3}>
+                <Grid item xs={12}>
+                    <Paper className={classes.paper}>Testing Material-UI, what on earth is going on :)
+                    </Paper>
+                </Grid>
+                <Grid item xs={6}>
+                    <Paper className={classes.paper}>
+                        <Button variant="contained" color={btnColor} onClick={changeColor}>
+                        Change colors!
+                        </Button>
+                    </Paper>
+                </Grid>
+                <Grid item xs={6}>
+                    <Paper className={classes.paper}>
+                        <Button variant="contained" color={btnColor} onClick={changeColor}>
+                        Change colors!
+                        </Button>
+                    </Paper>
+                </Grid>
+                <Grid item xs={4}>
+                    <Paper className={classes.paper}>xs=4</Paper>
+                </Grid>
+                <Grid item xs={4}>
+                    <Paper className={classes.paper}>xs=4</Paper>
+                </Grid>
+                <Grid item xs={4}>
+                    <Paper className={classes.paper}>xs=4</Paper>
+                </Grid>
+                <Grid item xs={3}>
+                    <Paper className={classes.paper}>Typing directly in the Paper</Paper>
+                </Grid>
+                <Grid item xs={3}>
+                    <Paper className={classes.paper}>
+                        <div className="">
+                            Typing in a div in the paper
+                        </div>
+                    </Paper>
+                </Grid>
+                <Grid item xs={3}>
+                    <Grid container spacing={3}>
+                        <Grid item xs={6}>
+                            <Paper className={classes.paper}>
+                                To put a grid within your grid, put a container within the item, then an item in that container
+                            </Paper>
+                        </Grid>
+                        <Grid item xs={6}>
+                            <Paper className={classes.paper}>
+                                Grid item in a grid container in a grid item in a grid container
+                            </Paper>
+                        </Grid>
+                    </Grid>
+                </Grid>
+                <Grid item xs={3}>
+                    <Paper className={classes.paper}>xs=3</Paper>
+                </Grid>
+            </Grid>
+        </div>
     );
 }
